13b891eab4585fa1e999361b066021d72492d87b
Homer6-dev <60720408+Homer6-dev@users.noreply.github.com>
2021-07-15 14:57:07 -0300

    Update Exercicio_7_3.java

--- aula/src/me/homerorion/br/Exercicio_7_3.java
+++ aula/src/me/homerorion/br/Exercicio_7_3.java
@@ -3,16 +3,20 @@ package me.homerorion.br;
 import javax.swing.JOptionPane;
 
 public class Aula7_1 {
+	
+	/*03 - FaÃ§a um programa em Java que leia idade de uma pessoa expressa em anos, meses e 
+	dias e escrever a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 
+	dias e mÃªs com 30 dias. */
 
 	public static void main(String[] args) {
 		int ano, mes, dia;
 		JOptionPane.showMessageDialog(null,
-				"Esse programa pedirá sua idade em anos, meses e dias respectivamente. Clique em ok para prosseguir \n");
+				"Esse programa pedirÃ¡ sua idade em anos, meses e dias respectivamente. Clique em ok para prosseguir \n");
 		ano = Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade em anos"));
-		mes = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do mês"));
+		mes = Integer.parseInt(JOptionPane.showInputDialog("Digite o nÃºmero do mÃªs"));
 		dia = Integer.parseInt(JOptionPane.showInputDialog("Digite o dia"));
 		int calc = (ano * 365) + (mes * 30) + dia;
-		JOptionPane.showMessageDialog(null, "Sua idade em dias é: " + calc);
+		JOptionPane.showMessageDialog(null, "Sua idade em dias Ã©: " + calc);
 	}
 
 }
