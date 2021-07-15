52760851af3bc5dbf099c82ace2b8a0741db0b37
Homer6-dev <60720408+Homer6-dev@users.noreply.github.com>
2021-07-15 14:58:28 -0300

    Update Exercicio_7_2.java

--- aula/src/me/homerorion/br/Exercicio_7_2.java
+++ aula/src/me/homerorion/br/Exercicio_7_2.java
@@ -3,22 +3,24 @@ package me.homerorion.br;
 import java.util.Scanner;
 
 public class Aula7_2 {
-
+	/*02 - Escreva um programa em Java para ler as dimens√µes de um ret√¢ngulo (base e altura), 
+		calcular e escrever a √°rea do ret√¢ngulo. */
+	
 	public static void main(String[] args) {
 
 		Scanner teclado = new Scanner(System.in);
 
 		double num1, num2, area;
 
-		System.out.println("Digite a base do ret‚ngulo: ");
+		System.out.println("Digite a base do ret√¢ngulo: ");
 		num1 = teclado.nextDouble();
 
-		System.out.println("Digite a altura do ret‚ngulo: ");
+		System.out.println("Digite a altura do ret√¢ngulo: ");
 		num2 = teclado.nextDouble();
 
 		area = (num1 * num2);
 
-		System.out.println("A ·rea do ret‚ngulo È: " + area);
+		System.out.println("A √°rea do ret√¢ngulo √©: " + area);
 
 		teclado.close();
 
