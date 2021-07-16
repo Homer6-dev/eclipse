0a5c561d177743bf4c9b99b983c17376bb9c1bea
Homer6-dev <60720408+Homer6-dev@users.noreply.github.com>
2021-07-16 13:17:32 -0300

    Update Exercicio_7_1.java

--- aula/src/me/homerorion/br/Exercicio_7_1.java
+++ aula/src/me/homerorion/br/Exercicio_7_1.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Exercicio_7_1 {
 
 	public static void main(String[] args) {
-
+	
+		/*01 - Escreva um algoritmo em pseudocÃ³digo e depois o transcreva para Java, o algoritmo 
+			deve LER um valor (do teclado) e ESCREVER (na tela) o seu antecessor.*/
+		
 		Scanner sc = new Scanner(System.in);
 
 		int num1;
 
-		System.out.println("Digite um número: ");
+		System.out.println("Digite um nÃºmero: ");
 		num1 = sc.nextInt();
 
-		System.out.println("\nO antecessor de" + num1 + "é o" + (num1 - 1) + ".");
+		System.out.println("\nO antecessor de" + num1 + "Ã© o" + (num1 - 1) + ".");
 
 		sc.close();
 	}
